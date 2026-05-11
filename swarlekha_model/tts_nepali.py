feda26eb35861d992689fe56e937fcce7b976c70
bishal2059 <bishal.sap21@gmail.com>
2026-05-11 23:01:18 +0545

    fix token limit

--- swarlekha_model/tts_nepali.py
+++ swarlekha_model/tts_nepali.py
@@ -302,7 +302,7 @@ class SwarlekhaNepaliTTS:
             speech_tokens = self.t3.inference(
                 t3_cond=self.conds.t3,
                 text_tokens=text_tokens,
-                max_new_tokens=100,  # TODO: use the value in config
+                max_new_tokens=1000,  # TODO: use the value in config
                 temperature=temperature,
                 cfg_weight=cfg_weight,
                 repetition_penalty=repetition_penalty,
